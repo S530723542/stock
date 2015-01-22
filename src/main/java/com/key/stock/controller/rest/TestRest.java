927550ff5c943940b7cd4d64662986f17dfa0e8b
jishen.yjs <jishen.yjs@alibaba-inc.com>
2015-01-22 17:13:58 +0800

    can get json

--- src/main/java/com/key/stock/controller/rest/TestRest.java
+++ src/main/java/com/key/stock/controller/rest/TestRest.java
@@ -1,15 +1,35 @@
 package com.key.stock.controller.rest;
 
-import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.key.stock.pojo.StockVO;
+import com.key.tools.common.RestResult;
+
+
+@RequestMapping("/key/web/")
 @Controller
 public class TestRest
 {
 	 
-	@ResponseBody
-	@RequestMapping("/ajax") 
-	public Object ajax(HttpServletRequest request)
+	@RequestMapping(value = "test", method = RequestMethod.GET)
+	public @ResponseBody RestResult<StockVO> createUserResource(
+			ModelMap model,
+			HttpSession session,
+			@RequestParam(value = "test", required = true) String test
+	)
 	{
 		
+		StockVO stockVO=new StockVO();
+		stockVO.setName(test);
+		RestResult<StockVO> restResult=new RestResult<StockVO>();
+		restResult.setData(stockVO);
+		return restResult;
 	}
 }
